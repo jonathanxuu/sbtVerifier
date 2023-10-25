da757fb4809c802cfd96daf3ac899d15f9865387
J <1410101208@qq.com>
2023-10-25 17:10:53 +0800

    Add EIP712 sig for KYA RiskLevel, wait to be test

--- src/index.ts
+++ src/index.ts
@@ -8,29 +8,28 @@ import {
 } from "@zcloak/crypto";
 import { caclculateDigest } from "./digestHandler";
 import { verify_digest_signature } from "./didHandler";
-import { eip712_sign } from "./signatureHandler";
+import { eip712_sign_kyc } from "./signatureHandler";
 import { Keyring } from "@zcloak/keyring";
 import { u8aToHex, hexToU8a } from "@polkadot/util"
 import { fromMnemonic } from "@zcloak/did/keys";
 
 // == phase 0: ZKP Generated (Generated in zkID Wallet, send to Server To Verify)  =====
 // The following metadata should be passed from web to server
-let user_did: DidUrl = "did:zk:0x11f8b77F34FCF14B7095BF5228Ac0606324E82D1";
+let userDid: DidUrl = "did:zk:0x11f8b77F34FCF14B7095BF5228Ac0606324E82D1";
 let ctype: HexString =
     "0x8a841a46b6e683a2b63c995f23a5590c946731007b451209711a239f2030a387";
-let vc_version: VerifiableCredentialVersion = "1";
-let issuance_date: number = 1697527231422;
-let expiration_date: number = 0;
+let vcVersion: VerifiableCredentialVersion = "1";
+let issuanceDate: number = 1697527231422;
+let expirationDate: number = 0;
 
-let attester_did: DidUrl = "did:zk:0xFeDE01Ff4402e35c6f6d20De9821d64bDF4Ba563";
-let attester_proof: Proof = {
+let attesterDid: DidUrl = "did:zk:0xFeDE01Ff4402e35c6f6d20De9821d64bDF4Ba563";
+let attesterProof: Proof = {
     type: 'EcdsaSecp256k1SignatureEip191',
     created: 1697527231429,
     verificationMethod: 'did:zk:0xFeDE01Ff4402e35c6f6d20De9821d64bDF4Ba563#key-0',
     proofPurpose: 'assertionMethod',
     proofValue: 'z8j93B63TCHAXSrAHDyydGXDCkTAg28XZHFXov3MJ7iMstihNFhRvYCTUBxaQD4qfFrmpGjAqjHcSkXJXuauYSoVwv'
 };
-
 let claimUserEthAddr: string = '05476EE9235335ADd2e50c09B2D16a3A2cC4ebEC';
 let claimStatus: number = 1;
 let chainID: number = 420;
@@ -38,30 +37,41 @@ let contractAddr: string = '0xe7366703cE41FfEfd0f6890ec484280Dc88B543b';
 
 // let timestamp: number =  dayjs().toDate().getTime();
 let timestamp: number = 1697708475764;
-
+let network: string = "Ethereum";
 initCrypto().then(async () => {
     const result = await kyc_verifier(
-        user_did,
+        userDid,
+        claimUserEthAddr,
         ctype,
-        vc_version,
-        issuance_date,
-        expiration_date,
-        attester_did,
-        attester_proof
+        vcVersion,
+        issuanceDate,
+        expirationDate,
+        attesterDid,
+        attesterProof,
+        chainID,
+        contractAddr,
+        false,
+        network
     );
     console.log(result);
 }
 );
 
+
 // ================================== Main Function ========================================
 async function kyc_verifier(
-    user_did: DidUrl,
+    userDid: DidUrl,
+    userOnChainAddress: string,
     ctype: HexString,
-    vc_version: VerifiableCredentialVersion,
+    vcVersion: VerifiableCredentialVersion,
     issuanceDate: number,
     expirationDate: number,
-    attester_did: DidUrl,
-    attester_proof: Proof,
+    attesterDid: DidUrl,
+    attesterProof: Proof,
+    chainID: number,
+    contractAddress: string,
+    isKYA: boolean,
+    network: string
 ): Promise<[Uint8Array]> {
     // ============= phase 1: check VC validity ================================
     // isVC? || computeRoothash, computeDigest, checkSignature || notCheck
@@ -70,18 +80,18 @@ async function kyc_verifier(
 
     const digest: HexString = caclculateDigest(
         roothash,
-        user_did,
+        userDid,
         issuanceDate,
         expirationDate,
         ctype,
-        vc_version
+        vcVersion
     );
 
     const signature_verify_result: boolean = await verify_digest_signature(
-        attester_did,
-        attester_proof,
+        attesterDid,
+        attesterProof,
         digest,
-        vc_version
+        vcVersion
     );
 
     // ========== phase 2: Verifier should make a signature for the whole process(text) ====
@@ -95,20 +105,50 @@ async function kyc_verifier(
     const controllerPath = `/m/44'/60'/0'/0/0`;
     const controller = testKeyring.addFromMnemonic(mnemonic, controllerPath, 'ecdsa');
     console.log(did.identifier)
-    let verifier_signature: Uint8Array = eip712_sign(
-        user_did,
-        ctype,
-        controller,
-        issuance_date,
-        expiration_date,
-        claimUserEthAddr,
-        claimStatus,
-        chainID,
-        contractAddr,
-        timestamp
-    );
+
+    let verifier_signature: Uint8Array = new Uint8Array();
+
+    if (isKYA) {
+        const kya_result = fetch_kya_result(network, userOnChainAddress);
+        verifier_signature = eip712_sign_kyc(
+            userDid,
+            ctype,
+            controller,
+            issuanceDate,
+            expirationDate,
+            claimUserEthAddr,
+            claimStatus,
+            chainID,
+            contractAddress,
+            timestamp,
+            kya_result
+        );
+    } else {
+        verifier_signature = eip712_sign_kyc(
+            userDid,
+            ctype,
+            controller,
+            issuanceDate,
+            expirationDate,
+            claimUserEthAddr,
+            claimStatus,
+            chainID,
+            contractAddress,
+            timestamp
+        );
+    }
     console.log(u8aToHex(verifier_signature))
     return [verifier_signature];
 }
 
 
+function fetch_kya_result(network: string, address: string): number {
+    //  curl --location --request POST 'https://api.chaintool.ai/kya/v2/risk_score?network=Ethereum&address=0x3d2a23ead4a6e2b8ff973250e56d70cc78353c50' \
+    //--header 'Content-Type: application/json' \
+    // --header 'TOKEN: {your token}
+
+    // parse curl result to get the Risk Score
+
+    // return the KYA Risk Score * 10; suggest the curl result is 33
+    return 33;
+}
--- src/signatureHandler.ts
+++ src/signatureHandler.ts
@@ -6,7 +6,7 @@ import { KeyringPair } from "@zcloak/keyring/types"
 import { hexToU8a, isU8a, u8aConcat, u8aToBuffer, u8aToU8a } from '@polkadot/util';
 const EIP_191_PREFIX = hexToU8a('0x1901');
 
-export function eip712_sign(
+export function eip712_sign_kyc(
     recipient: DidUrl,
     ctype: HexString,
     keypair: KeyringPair,
@@ -16,9 +16,10 @@ export function eip712_sign(
     claimStatus: number,
     chainID: number,
     contractAddr: string,
-    timestamp: number
+    timestamp: number,
+    kyaResult?: number
 ): Uint8Array {
-    let typedData = constrcut_typedData(
+    let typedData = construct_typedData(
         recipient,
         ctype,
         issuance_date,
@@ -27,12 +28,9 @@ export function eip712_sign(
         claimStatus,
         chainID,
         contractAddr,
-        timestamp
+        timestamp,
+        kyaResult
     );
-
-    // const message0 = u8aConcat(EIP_191_PREFIX, structHash(typedData, 'EIP712Domain', typedData.domain), structHash(typedData, typedData.primaryType, typedData.message));
-    // console.log("u8a is", u8aToHex(encodeData(typedData, typedData.primaryType, typedData.message)));
-    // console.log(u8aToHex(u8aConcat(EIP_191_PREFIX, structHash(typedData, 'EIP712Domain', typedData.domain), structHash(typedData, typedData.primaryType, typedData.message))));
 
     const message = getMessage(typedData, true);
     const signature = keypair.sign(message);
@@ -40,7 +38,7 @@ export function eip712_sign(
 }
 
 
-function constrcut_typedData(
+function construct_typedData(
     recipient: DidUrl,
     ctype: HexString,
     issuance_date: number,
@@ -49,53 +47,104 @@ function constrcut_typedData(
     claimStatus: number,
     chainID: number,
     contractAddr: string,
-    timestamp: number
+    timestamp: number,
+    kyaResult?: number
 ) {
     const recipient_address: string = parseDid(recipient).identifier;
 
-    const typedData = {
-        types: {
-            EIP712Domain: [
-                { name: 'name', type: 'string' },
-                { name: 'version', type: 'string' },
-                { name: 'chainId', type: 'uint256' },
-                { name: 'verifyingContract', type: 'address' }
-            ],
-            signature: [
-                { name: 'userDID', type: 'address' },
-                { name: 'ctype', type: 'bytes32' },
-                { name: 'issuanceDate', type: 'bytes' },
-                { name: 'expirationDate', type: 'bytes' },
-                { name: 'claimUserEthAddr', type: 'string' },
-                { name: 'claimStatus', type: 'uint256' },
-                { name: 'chainID', type: 'uint256' },
-                { name: 'contractAddr', type: 'address' },
-                { name: 'timestamp', type: 'uint256' },
-            ]
-        },
-        primaryType: 'signature',
-        // change when the final contract is deployed
-        domain: {
-            name: 'SigVerify',
-            version: '0',
-            // chainId: 4,
-            chainId: chainID, // hardhat test chainId
-            verifyingContract: contractAddr // hardhat test contract
-        },
-        message: {
-            userDID: recipient_address,
-            ctype: ctype,
-            issuanceDate: pad(issuance_date.toString(16)),
-            expirationDate: pad(expiration_date.toString(16)),
-            claimUserEthAddr: claimUserEthAddr,
-            claimStatus: claimStatus,
-            chainID: chainID,
-            contractAddr: contractAddr,
-            timestamp: timestamp
+    if (kyaResult == null) {
+        const typedData = {
+            types: {
+                EIP712Domain: [
+                    { name: 'name', type: 'string' },
+                    { name: 'version', type: 'string' },
+                    { name: 'chainId', type: 'uint256' },
+                    { name: 'verifyingContract', type: 'address' }
+                ],
+                signature: [
+                    { name: 'userDID', type: 'address' },
+                    { name: 'ctype', type: 'bytes32' },
+                    { name: 'issuanceDate', type: 'bytes' },
+                    { name: 'expirationDate', type: 'bytes' },
+                    { name: 'claimUserEthAddr', type: 'string' },
+                    { name: 'claimStatus', type: 'uint256' },
+                    { name: 'chainID', type: 'uint256' },
+                    { name: 'contractAddr', type: 'address' },
+                    { name: 'timestamp', type: 'uint256' },
+                ]
+            },
+            primaryType: 'signature',
+            // change when the final contract is deployed
+            domain: {
+                name: 'SigVerify',
+                version: '0',
+                // chainId: 4,
+                chainId: chainID, // on which chainID to use
+                verifyingContract: "0x00351b372FB793D6Fa1902E6b2db2E6A7d8824E7" // zClock Verification Contract
+            },
+            message: {
+                userDID: recipient_address,
+                ctype: ctype,
+                issuanceDate: pad(issuance_date.toString(16)),
+                expirationDate: pad(expiration_date.toString(16)),
+                claimUserEthAddr: claimUserEthAddr,
+                claimStatus: claimStatus,
+                chainID: chainID,
+                contractAddr: contractAddr,
+                timestamp: timestamp
+            }
         }
+        console.log(typedData.message)
+        return typedData;
+    } else {
+        const typedData = {
+            types: {
+                EIP712Domain: [
+                    { name: 'name', type: 'string' },
+                    { name: 'version', type: 'string' },
+                    { name: 'chainId', type: 'uint256' },
+                    { name: 'verifyingContract', type: 'address' }
+                ],
+                signature: [
+                    { name: 'userDID', type: 'address' },
+                    { name: 'ctype', type: 'bytes32' },
+                    { name: 'issuanceDate', type: 'bytes' },
+                    { name: 'expirationDate', type: 'bytes' },
+                    { name: 'claimUserEthAddr', type: 'string' },
+                    { name: 'claimStatus', type: 'uint256' },
+                    { name: 'chainID', type: 'uint256' },
+                    { name: 'contractAddr', type: 'address' },
+                    { name: 'timestamp', type: 'uint256' },
+                    { name: 'kyaResult', type: 'uint256' },
+
+                ]
+            },
+            primaryType: 'signature',
+            // change when the final contract is deployed
+            domain: {
+                name: 'SigVerify',
+                version: '0',
+                // chainId: 4,
+                chainId: chainID, // on which chainID to use
+                verifyingContract: "0x00351b372FB793D6Fa1902E6b2db2E6A7d8824E7" // zClock Verification Contract
+            },
+            message: {
+                userDID: recipient_address,
+                ctype: ctype,
+                issuanceDate: pad(issuance_date.toString(16)),
+                expirationDate: pad(expiration_date.toString(16)),
+                claimUserEthAddr: claimUserEthAddr,
+                claimStatus: claimStatus,
+                chainID: chainID,
+                contractAddr: contractAddr,
+                timestamp: timestamp,
+                kyaResult: kyaResult
+            }
+        }
+        console.log(typedData.message)
+        return typedData;
     }
-    console.log(typedData.message)
-    return typedData;
+
 };
 
 function pad(input: string): string {
